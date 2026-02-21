f47412d2c218ef94c728e1042f47adba8c6d0e5f
pjha26 <praj80624@gmail.com>
2026-02-21 23:41:04 +0530

    modified:   server/routes/authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getMe } = require('../controllers/authController');
+const { registerUser, loginUser, getMe, updateUserProfile, updateUserPassword } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protect, getMe);
+router.put('/profile', protect, updateUserProfile);
+router.put('/password', protect, updateUserPassword);
 
 module.exports = router;
